dca25e4ca22a1940c89cc4d430b43960d3547307
plastyji <41068372+plastyji@users.noreply.github.com>
2023-08-06 11:46:07 -0300

    Update funcoes.py

--- PROJETO PYTHON/funcoes.py
+++ PROJETO PYTHON/funcoes.py
@@ -4,22 +4,27 @@ from tkinter import messagebox
 
 def connectionDB():
     conn = db.connect('school.db')
+
     return conn
 
 
-def register_studant(name, cpf , date_born, name_mother, phone,email):
+def register_student(name, cpf, date_born, name_mother, phone, email):
     conn = connectionDB()
     cursor = conn.cursor()
     try:
-        cursor.execute('''SELECT cpf FROM Alunos
-            WHERE cpf = ?''',(cpf,))
+
+        cursor.execute('''SELECT cpf FROM student
+            WHERE cpf = ?''', (cpf,))
         CPF_REGISTER = cursor.fetchone()
+
         if CPF_REGISTER:
             conn.close()
             return False, "O aluno ja é cadastrado"
-        cursor.execute('''INSERT INTO Student (cpf, name, date_born, name_mother, email, phone) values (?,?,?,?,?,?)''',(cpf, name, date_born,name_mother,email,phone))
+        cursor.execute('''INSERT INTO Student (cpf, name, date_born, name_mother, email, phone) values (?,?,?,?,?,?)''',(cpf, name, date_born, name_mother,email,phone))
+        print("ok")
         conn.commit()
         conn.close()
+        messagebox.showinfo(title="Sucesso", message="Sucesso ao cadastrar o Estudante")
         return True, "O aluno foi cadastrado com sucesso"
 
     except Exception as e:
@@ -59,19 +64,18 @@ def register_course(name_course, shift, duration):
     except Exception as e:
         conn.close()
         return False,  f'Erro ao cadastrar curso. Erro: {str(e)}'
-
 def register_class(id_course, cpf_teacher):
     conn = connectionDB()
     cursor = conn.cursor()
     try:
         cursor.execute('''select id from course where id=?''', (id_course,))
-        course  =  cursor.fetchone()
+        course = cursor.fetchone()
         cursor.execute(''' select cpf from teacher where cpf=?''',(cpf_teacher))
         teacher = cursor.fetchone()
         if not(course and teacher):
 
             conn.close()
-            return False, "Curso ou Professor não existe" \
+            return False, "Curso ou Professor não existe"
         cursor.execute(''' INSERT INTO class(id_course, cpf_teacher ) value (?,?)''', (id_course, cpf_teacher))
         conn.commit()
         conn.close()
@@ -79,5 +83,133 @@ def register_class(id_course, cpf_teacher):
         conn.close()
         return False, f'Erro ao cadastrar a turma. ERRO{str(e)}'
 
+def register_student_class(id_studant, id_class):
+    conn = connectionDB()
+    cursor = conn.cursor()
+    try:
+        cursor.execute("Select cpf from student where cpf = ?", (id_studant,))
+        student_exist = cursor.fetchone()
 
-    except:
+        cursor.execute("Select id from class where id = ?", (id_class,))
+        class_exist = cursor.fetchone()
+
+        if not (student_exist, class_exist):
+            return False , "Aluno ou turma não encontrado."
+        cursor.execute('''INSERT INTO Studant_Class(id_studant, id_class)
+        values (?, ?)''', (id_studant, id_class))
+        conn.commit()
+        return True, "Aluno cadastrado na turma com sucesso."
+
+    except Exception as e:
+        return False, f"Erro ao cadastrar aluno na turma: {str(e)}"
+    finally:
+        conn.close()
+
+def view_student():
+    conn = connectionDB()
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        SELECT * FROM Student
+    ''')
+    student = cursor.fetchall()
+    conn.close()
+
+    return student
+
+
+def view_teacher():
+    conn = connectionDB()
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        SELECT * FROM teacher
+    ''')
+    teacher = cursor.fetchall()
+    conn.close()
+
+    return teacher
+
+def view_course():
+    conn = connectionDB()
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        SELECT * FROM Course
+    ''')
+    Couser = cursor.fetchall()
+    conn.close()
+
+    return Couser
+def view_class():
+    conn = connectionDB()
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        SELECT * FROM Class
+    ''')
+    clas = cursor.fetchall()
+    conn.close()
+    return clas
+
+#Atualização#
+def atualization_people(name, cpf, date_born, name_mother, phone, email):
+    conn = connectionDB()
+    cursor = conn.cursor()
+
+    try:
+        cursor.execute(''' SELECT cpf FROM Student where cpf = ?''', (cpf,))
+        student_exist = cursor.fetchone()
+        cursor.execute(''' SELECT cpf FROM teacher where cpf = ?''', (cpf,))
+        teacher_exist = cursor.fetchone()
+
+        if not (student_exist or teacher_exist):
+            return False, "Cpf não encontrado"
+
+        if(student_exist):
+            cursor.execute('''
+                    UPDATE student SET name = ? , cpf = ?,
+                    date_born = ? , name_mother = ? , phone = ? , email = ? WHERE cpf = ?
+            ''', (name, cpf, date_born, name_mother, phone, email,cpf))
+
+        elif(teacher_exist):
+            cursor.execute('''
+                                UPDATE teacher SET name = ? , cpf = ?,
+                                date_born = ? , name_mother = ? , phone = ? , email = ? WHERE cpf = ?
+                        ''', (name, cpf, date_born, name_mother, phone, email, cpf))
+        conn.commit()
+
+
+        return True, "Pessoa Atualizada Com Sucesso"
+    except Exception as e:
+
+        return  False, f'Erro ao atualizar pessoa. {str(e)}'
+    finally:
+        conn.close()
+
+
+def delete_people(cpf):
+    conn = connectionDB()
+    cursor = conn.cursor()
+    try:
+        cursor.execute(''' SELECT cpf FROM Student where cpf = ?''', (cpf,))
+        student_exist = cursor.fetchone()
+        cursor.execute(''' SELECT cpf FROM teacher where cpf = ?''', (cpf,))
+        teacher_exist = cursor.fetchone()
+
+        if not (student_exist or teacher_exist):
+            return False, "Cpf não encontrado"
+        if(student_exist):
+            cursor.execute('DELETE FROM Student where cpf = ?'(cpf,))
+
+        elif(teacher_exist):
+            cursor.execute('DELETE FROM teacher where cpf = ? '(cpf,))
+
+        conn.commit()
+
+        return True, "Pessoa Excluida Com Sucesso"
+    except Exception as e:
+        return False, f"Erro ao excluir pessoa.{str(e)} "
+    finally:
+        conn.close()
+
